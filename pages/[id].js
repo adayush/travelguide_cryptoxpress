7900147076cc177cfc7ced9189ce1ff34c157d2a
adayush <adayush0@gmail.com>
2022-11-29 09:19:13 +0530

    edited nextjs.config

--- pages/[id].js
+++ pages/[id].js
@@ -2,26 +2,28 @@ import { getAllPlacesIds, getPlaceData } from '../lib/places';
 import Layout from '../components/layout';
 import Placelayout from '../components/placelayout';
 
-export default function Place({ placeData }) {
+export default function Place({ placeData, images }) {
   return <Layout>
-    <Placelayout placeData={placeData} />
+    <Placelayout placeData={placeData} images={images} />
   </Layout>;
 }
 
+export async function getStaticPaths() {
+  const paths = getAllPlacesIds();
+  
+  return {
+    paths,
+    fallback: false,
+  };
+
+}
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
   const placeData = getPlaceData(params.id)
+  
   return {
     props: {
       placeData
     }
   }
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPlacesIds();
-  return {
-    paths,
-    fallback: false,
-  };
 }
